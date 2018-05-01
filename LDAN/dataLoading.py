54fee4d140c9f5518671881250c19212bba09add
bhushan <bhushan.s.94@gmail.com>
2018-05-01 19:58:08 -0400

    Refactoring code for further implementation

---
+++ LDAN/dataLoading.py
@@ -0,0 +1,133 @@
+from torch.utils.data import Dataset, DataLoader
+import os
+import h5py
+import numpy as np
+from utils import PRINT
+from torchvision import transforms
+import torch
+
+# Normalize data while loading
+class CustomDataSetLoader(Dataset):
+    def __init__(self, inData, transform = None):
+        self.data = inData
+        self.transform = transform
+        #normalize here or in __getitem__
+
+    def __getitem__(self, index):
+        data = self.data[index]
+        if self.transform is not None:
+            data = self.transform(data)
+        data = data.permute(1, 2, 0)
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+
+def get_h5_file_names(path):
+    h5Files = []
+    for file in os.listdir(path):
+        h5Files.append(path+file)
+    return h5Files
+
+# Following in Synthetic image data loading
+# LDAN stores multiple h5 files containing two synthetic image
+# and their corresponding SH
+# Following function will process thos into one and return
+def load_synthetic_ldan_data(path, batch_size = 64):
+    h5Files = get_h5_file_names(path)
+    if len(h5Files) == 0:
+        PRINT('NO H5 FILE FOUND FOR SYNTHETIC IMAGES', 'WARNING')
+        return None, None, None
+
+    h5 = h5py.File(h5Files[0], 'r')
+    syn1 = h5['data_1']
+    syn2 = h5['data_2']
+    dlabel = h5['label']
+    synIm1 = np.array(syn1[:,:,:]) #np.swapaxes(np.array(dset[:,:,:]), 1, 3)
+    synIm2 = np.array(syn2[:,:,:]) #np.swapaxes(np.array(dset2[:,:,:]), 1, 3)
+    synLabel = np.array(dlabel)
+
+    totalSets = len(h5Files)
+    PRINT(totalSets)
+    for i in range(1, totalSets):
+        h5 = h5py.File(h5Files[i], 'r')
+        syn1 = h5['data_1']
+        syn2 = h5['data_2']
+        dlabel = h5['label']
+        synIm1 = np.concatenate((synIm1, np.array(syn1[:,:,:]))) #np.swapaxes(np.array(dset[:,:,:]), 1, 3)
+        synIm2 = np.concatenate((synIm2, np.array(syn2[:,:,:]))) #np.swapaxes(np.array(dset2[:,:,:]), 1, 3)
+        synLabel = np.concatenate((synLabel, np.array(dlabel)))
+        PRINT(synIm1.shape)
+        PRINT(synIm2.shape)
+        PRINT(synLabel.shape)
+
+    # Synthetic data was stored from Matlab and hence needs to move the axis
+    syn1 = np.moveaxis(synIm1, 1, 3)
+    syn1 = np.moveaxis(syn1, 1, 2)
+    syn1 = np.moveaxis(syn1, 2, 3)
+
+    syn2 = np.moveaxis(synIm2, 1, 3)
+    syn2 = np.moveaxis(syn2, 1, 2)
+    syn2 = np.moveaxis(syn2, 2, 3)
+
+    transform = transforms.Compose([
+            #transforms.Resize(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+
+    # Dataset
+    syn1 = CustomDataSetLoader(syn1, transform = transform)
+    syn2 = CustomDataSetLoader(syn2, transform = transform)
+
+    # Data Loader for synthetic images
+    synImage1 = torch.utils.data.DataLoader(syn1, batch_size = batch_size)
+    synImage2 = torch.utils.data.DataLoader(syn2, batch_size = batch_size)
+    synLabel = torch.utils.data.DataLoader(synLabel, batch_size = batch_size)
+
+    PRINT('Loading Synthetic Images Completed')
+    # LDAN works on two frontal pose having same SH: Returning both image set
+    return synImage1, synImage2, synLabel
+
+def load_real_images_celebA(path, batch_size = 64):
+    h5Files = get_h5_file_names(path)
+    if len(h5Files) == 0:
+        PRINT('NO H5 FILE FOUND FOR SYNTHETIC IMAGES', 'WARNING')
+        return None
+
+    h5 = h5py.File(h5Files[0], 'r')
+    rImage = h5['/Image']
+    lighting = h5['/Lighting']
+    normal = h5['/Normal']
+    shading = h5['/Shading']
+
+    # Following are not need for current experiment
+    # PLEASE UNCOMMENT IF YOU NEED
+    # height = h5['/Height']
+    # reflectance = h5['/Reflectance']
+    # finalLoss = h5['/FinalLoss']
+
+    # Transforms being used
+    transform = transforms.Compose([
+            #transforms.Resize(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+    # Custom image dataset
+    # Normal and Shading is already normalized by SIRFS method
+    # So, Normalize only real images
+    rImage = CustomDataSetLoader(rImage, transform = transform)
+
+    realImage = torch.utils.data.DataLoader(rImage, batch_size= batch_size, shuffle = False)
+    realSH = torch.utils.data.DataLoader(lighting, batch_size= batch_size, shuffle = False)
+    rNormal = torch.utils.data.DataLoader(normal, batch_size= batch_size, shuffle = False)
+    rShading = torch.utils.data.DataLoader(shading, batch_size= batch_size, shuffle = False)
+
+    PRINT('Loading CelebA Real Images Completed')
+    # Following are not need for current experiment
+    # PLEASE UNCOMMENT IF YOU NEED
+    # rHeight = torch.utils.data.DataLoader(height, batch_size= batch_size, shuffle = False)
+    # rReflectance = torch.utils.data.DataLoader(reflectance, batch_size= batch_size, shuffle = False)
+    # rFinalLoss = torch.utils.data.DataLoader(finalLoss, batch_size= batch_size, shuffle = False)
+    return realImage, rNormal, realSH, rShading
---
+++ LDAN/lossfunctions.py
@@ -0,0 +1,19 @@
+# Parameters being used by loss functions
+MU = 50.0
+RHO = 50.0
+LAMDA = 0.5
+
+def regression_loss_synthetic(syn1, label):
+    return (syn1 - label)**2
+
+def feature_loss(syn1, syn2):
+    return (syn1 - syn2)**2
+
+def regression_loss(syn1, syn2, label):
+    rLoss = regression_loss_synthetic(syn1, label) + regression_loss_synthetic(syn2, label)
+    fLoss = feature_loss(syn1, syn2)
+    loss = RHO * rLoss + LAMDA * fLoss
+    return loss.sum()
+
+def GAN_loss(vals):
+    return vals.mean()
